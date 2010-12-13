a1a89cd663ebea709b9febc86420b7fedbe38556
Arto Iijalainen <arto.iijalainen@gmail.com>
2010-12-13 12:19:25 +0200

    Added a check for missing status file.

--- connection_checker.sh
+++ connection_checker.sh
@@ -1,8 +1,16 @@
+### Init ###
 status_file='/tmp/conection_status'
+if [ ! -f $status_file ]
+then
+	echo "Status file does not exist. System restarted?"
+	echo 'unspecified' > $status_file
+fi
 
 prev_status=$(head -1 $status_file >&1)
 date_stamp=$(date '+[%Y-%m-%d %H:%M:%S]' >&1)
 
+
+### Action ###
 output=$(wget --timeout=5 --spider http://192.168.0.253 2>&1)
 if [ $? -eq 0 ]
 then
